a7041007a0a37dcd60b66c433706ea4e93a12b7e
Sebi2k <96271751+Sebi2k@users.noreply.github.com>
2024-03-10 23:33:01 -0700

    added movement

---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get the input values for horizontal and vertical axes
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // Calculate the movement direction
+        Vector3 movement = new Vector3(horizontalInput, verticalInput, 0f);
+
+        // Move the character
+        transform.Translate(movement * speed * Time.deltaTime);
+    }
+}
